9acc383bc8fff585698cb01f01f2ac5b447ba8a6
yukaimacew <150079218+yukaimacew@users.noreply.github.com>
2023-11-20 01:28:26 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+(function ($) {
+  
+
+  $(window).on('load hashchange', function(){
+    
+    // First hide all content regions, then show the content-region specified in the URL hash 
+    $('.content-region').hide();
+    
+    // Remove any active classes on the main-menu
+    $('.main-menu a').removeClass('active');
+    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
+    
+    // Now show the region specified in the URL hash
+    $(region).show();
+    
+    // Highlight the menu link associated with this region by adding the .active CSS class
+    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
