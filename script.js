68cd70bf7bf6743e92378cd2131c7b17dfb7dc34
yukaimacew <150079218+yukaimacew@users.noreply.github.com>
2023-11-21 00:54:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
     // Now show the region specified in the URL hash
     $(region).show();
     document.body.style.backgroundColor = 'transparent';
-    document.body.style.backgroundImage = "url('mirage.jpg') center center no-repeat fixed";
+    document.body.style.backgroundImage = "url('mirage.jpg')";
     document.body.style.backgroundSize = "cover";
     
     // Highlight the menu link associated with this region by adding the .active CSS class
