17dced9e7e195f9118da17dee3414020b6be63f5
yukaimacew <150079218+yukaimacew@users.noreply.github.com>
2023-11-21 02:48:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,25 +10,13 @@
     // Remove any active classes on the main-menu
     $('.main-menu a').removeClass('active');
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    var bkimg = region.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
-    console.log(bkimg);
+    var leaking = region.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+    console.log(leaking);
 
     
     // Now show the region specified in the URL hash
     $(region).show();
     
-    if(bkimg = "mirage"){
-      document.body.style.backgroundImage='none';
-      document.body.style.backgroundImage = "url('mirage.jpg')";
-    } else if (bkimg = "soraka"){
-      document.body.style.backgroundImage='none';
-      document.body.style.backgroundImage = "url('soraka.jpg')";
-    } else if (bkimg = "bidoof"){
-      document.body.style.backgroundImage='none';
-      document.body.style.backgroundImage = "url('bidoof.jpg')";
-    } else {
-      document.body.style.backgroundImage = 'none';
-    }
                
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
@@ -41,6 +29,7 @@ document.getElementById("sorakaBut").addEventListener("click", myFunction);
 document.getElementById("bidoofBut").addEventListener("click", myFunction);  
 
 function myFunction(){
+  console.log('CLICKED');
     document.body.style.backgroundImage = 'none';
     var region = location.hash.toString() || $('.main-menu a:first').attr('href');
     var bkimg = region.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
@@ -48,12 +37,16 @@ function myFunction(){
 
     if(bkimg = "mirage"){
       document.body.style.backgroundImage = "url('mirage.jpg')";
+      console.log("hihihi");
     } else if (bkimg = "soraka"){
       document.body.style.backgroundImage = "url('soraka.jpg')";
+      console.log("goodbye");
     } else if (bkimg = "bidoof"){
       document.body.style.backgroundImage = "url('bidoof.jpg')";
+      console.log("bibarel");
     } else {
       document.body.style.backgroundImage = 'none';
+      console.log("huhhuh");
     }
 }
 
