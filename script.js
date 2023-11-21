454aefc90781e503bcd6666d1e50f0a954ef2887
yukaimacew <150079218+yukaimacew@users.noreply.github.com>
2023-11-21 00:23:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
     
     // Now show the region specified in the URL hash
     $(region).show();
-    var bkimg = region + ".jpg";
+    document.body.style.backgroundImage = ["url" + "('" + region + ".jpg')"];
     
     // Highlight the menu link associated with this region by adding the .active CSS class
     $('.main-menu a[href="'+ region +'"]').addClass('active'); 
